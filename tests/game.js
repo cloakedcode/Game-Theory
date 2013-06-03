a81dc52cdff1b2ec50ac0005552a7e2a93245662
Alan Smith <alan@cloakedcode.com>
2013-06-03 15:48:53 -0400

    Unit tests are now passing.

--- tests/game.js
+++ tests/game.js
@@ -1,15 +1,12 @@
-game = require('../src/game.js');
+var game = require('../src/game.js');
 
 exports.available_games = function (test) {
     game.available_games(function (games) {
         test.expect(1);
         
-        console.log(games);
         test.ok(games.length > 0, 'Could not get list of games.');
         
         test.done();
 
     });
-
-    test.done();
 }
