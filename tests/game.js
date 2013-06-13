94c8e5eae1270e9c40c7b091b78cc131d10a3c86
Informatics <root@codepurple.goshen.edu>
2013-06-13 18:50:11 -0400

    Completed and passing new unit tests.

--- tests/game.js
+++ tests/game.js
@@ -1,11 +1,64 @@
-var game = require('../src/game.js');
+var Game = require(__dirname + '/../src/game.js')
+, fs = require('fs')
+
+exports.setUp = function (callback) {
+    fs.writeFileSync(__dirname + '/../games/nodeunit_test_game.js', "module.exports = exports = {name: 'Test', play: function (pair, callback) {callback()}, num_per_round: 2}");
+    
+    callback();
+}
+
+exports.tearDown = function (callback) {
+    fs.unlinkSync(__dirname + '/../games/nodeunit_test_game.js');
+
+    callback();
+}
+
+exports.create_game = function (test) {
+    test.expect(3);
+
+    test.throws(function () {Game()}, 'Did not throw error when created with no Game name.');
+    test.throws(function () {Game({name: 'sdflksjdf'})}, 'Did not throw error when created with invalid Game name.');
+    game = Game({name: 'Test'});
+    test.ok(game != undefined && game.name == 'Test', 'Did not create Game with vaild name.');
+
+    test.done();
+}
+
+exports.play = function (test) {
+    test.expect(1);
+
+    Player = {is_playing: false, set_status: function () {}, round_ended: function () {}};
+
+    game = Game({name: 'Test'});
+    game.play([Player, Player], function (game) {
+        test.expect(2);
+
+        test.ok(game.name == 'Test', 'Did not pass correct game to end_game callback.');
+        test.ok(Player.is_playing == false, 'Players not marked as not playing.');
+    });
+
+    test.ok(Player.is_playing, 'Players not marked as playing.');
+    test.done();
+}
 
 exports.available_games = function (test) {
-    game.available_games(function (games) {
+    Game.available_games(function (games) {
         test.expect(1);
         
-        console.log(games);
-        test.ok(games.length > 0, 'Could not get list of games.');
+        test.ok(games.length > 0, 'Could not get list of Games.');
+        
+        test.done();
+    });
+}
+
+exports.exists = function (test) {
+    test.expect(2);
+
+    Game.exists('Test', function (exists) {
+        test.ok(exists, 'Valid game reported as non-existent.');
+    });
+    Game.exists('sdlfkasjfd', function (exists) {
+        test.ifError(exists, 'Invalid game reported as existent.');
         
         test.done();
     });
--- tests/lobby.js
+++ tests/lobby.js
@@ -1,12 +1,87 @@
 var Lobby = require('../src/lobby.js'),
-Game = require('../src/game.js');
+Game = require('../src/game.js')
+, fs = require('fs')
+
+exports.setUp = function (callback) {
+    fs.writeFileSync(__dirname + '/../games/nodeunit_test_game.js', "module.exports = exports = {name: 'Test', play: function (pair, callback) {callback()}, num_per_round: 2}");
+    
+    callback();
+}
+
+exports.tearDown = function (callback) {
+    fs.unlinkSync(__dirname + '/../games/nodeunit_test_game.js');
+
+    callback();
+}
 
 exports.create_lobby = function (test) {
-    test.expect(3);
+    test.expect(2);
     
-    test.throws(function () {Lobby('tic-tac-toe')});
-    test.doesNotThrow(function () {Lobby(Game("rockpaperscissors"))});
-    test.ok(Lobby(Game("rockpaperscissors")), 'Could not create Lobby.');
+    test.throws(function () {Lobby('tic-tac-toe')}, 'Lobby created with invalid Game did not throw.');
+    test.doesNotThrow(function () {Lobby(Game({name: 'Test'}))}, 'Lobby created with valid Game threw.');
 
     test.done();
+}
+
+exports.add_player = function (test) {
+    lobby = Lobby(Game({name: 'Test'}));
+    player = {set_lobby: function (lobby) {this.lobby = lobby}, set_status: function () {}, name: 'fake'};
+
+    lobby.add_player(player);
+
+    test.expect(2);
+    test.ok(lobby.players.length == 1 && lobby.players[0] == player, 'Failed to add player to lobby.');
+    test.ok(player.lobby == lobby, 'Failed to set lobby of player.');
+    test.done();
+}
+
+exports.remove_player = function (test) {
+    lobby = Lobby(Game({name: 'Test'}));
+    player = {set_lobby: function (lobby) {this.lobby = lobby}, set_status: function () {}, name: 'fake'};
+
+    lobby.add_player(player);
+    lobby.remove_player(player);
+
+    test.expect(2);
+    test.ok(lobby.players.length == 0, 'Failed to remove player from lobby.');
+    test.ok(player.lobby == null, 'Failed to reset lobby of player.');
+    test.done();
+}
+
+exports.pair_players = function (test) {
+    game = Game({name: 'Test'});
+    lobby = Lobby(game);
+    player = {set_lobby: function (lobby) {this.lobby = lobby}, set_status: function () {}, round_ended: function () {}, name: 'fake'};
+    player2 = {set_lobby: function (lobby) {this.lobby = lobby}, set_status: function () {}, round_ended: function () {}, name: 'fake2'};
+    player3 = {set_lobby: function (lobby) {this.lobby = lobby}, set_status: function () {}, round_ended: function () {}, name: 'fake3'};
+
+    lobby.add_player(player);
+    lobby.add_player(player2);
+    lobby.add_player(player3);
+
+    lobby.pair_players();
+
+    test.expect(1);
+    test.ok(lobby.pairs.length == 3, 'Failed to pair players properly.');
+    test.done();
+}
+
+exports.run_game = function (test) {
+    game = Game({name: 'Test'});
+    lobby = Lobby(game);
+    player = {set_lobby: function (lobby) {this.lobby = lobby}, set_status: function () {}, round_ended: function () {}, name: 'fake', socket: true};
+    player2 = {set_lobby: function (lobby) {this.lobby = lobby}, set_status: function () {}, round_ended: function () {}, name: 'fake2', socket: true};
+
+    lobby.add_player(player);
+    lobby.add_player(player2);
+
+    lobby.run_game();
+
+    setTimeout(function (test, lobby, player) {
+        test.expect(3);
+        test.ok(lobby.pairs.length == 1, 'Failed to pair players.');
+        test.ok(player.is_playing, 'Player is not marked as playing.');
+        test.ok(lobby.is_running, 'Lobby is not marked as running.');
+        test.done();
+    }, 900, test, lobby, player);
 }
